e117e01d936baab56098b3fa462c2c1a676ca235
Andres Briñez <brinezlopez08@gmail.com>
2023-05-06 18:21:39 -0500

    Codigo para quitar el munu hamburguesa cuando haya determinado tamalo

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,19 +1,29 @@
 // selector
-let menu = document.querySelector('.hamburger');
+let menuHamburguer = document.querySelector('.hamburger');
 let body = document.querySelector('body');
 
-// method mostrarr y ocultar menu
+// method mostrar y ocultar menu
 function toggleMenu(event) {
 
     event.preventDefault();
 
-    body.classList.toggle('bodyNav');
+    body.classList.toggle('bodyNav'); // clase para que se ponga oscuso el fondo de la pagina
     this.classList.toggle('is-active');
 
     document.querySelector(".barraNavegación").classList.toggle("is_active");
     
 }
 
-// event
-menu.addEventListener('click', toggleMenu, false);
+// event para cuanso se haga click 
+menuHamburguer.addEventListener('click', toggleMenu, false);
 
+
+// evento cuando recarga la pantalla
+window.addEventListener('load', function(){
+
+    //cuando el tamaño del dispositivo mayor a 758 se debe agregar la clase is_active para que se muestre la barra de navegacion
+    if (window.innerWidth > 758) {
+        document.querySelector(".barraNavegación").classList.add("is_active");
+           
+}
+});
